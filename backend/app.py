e223032fed6cd9f5ddfe39b2b6de509558bb561e
jeff <zombie1864@gmail.com>
2021-03-17 09:20:37 -0400

    backend updated

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,12 @@
 from flask import Flask, json, jsonify
+
 app = Flask(__name__)
+
+def add_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] =  "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+    response.headers['Access-Control-Allow-Methods']=  "GET"
+    return response
 
 @app.route('/')
 def index():
@@ -9,8 +16,8 @@ def index():
 @app.route('/<index>')
 def getItem(index):
     with open("fixture.json", "r") as read_file:
-        developer = json.load(read_file)
-    res = developer["results"][int(index)]
+        fixture = json.load(read_file)
+    res = fixture["results"][int(index)]
     return jsonify(res)
 
 if __name__ == "__main__":
