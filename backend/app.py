11ea76925b2bc58d97818ccc530632702ef88004
jeff <zombie1864@gmail.com>
2021-03-16 21:32:20 -0400

    backend sends all data or a single data from dataset

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, json 
+from flask import Flask, json, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = json.loads(open('fixture.json').read())
+    data = json.loads(open('fixture.json').read()) # sends json file 
     return data
+
+@app.route('/<index>')
+def getItem(index):
+    with open("fixture.json", "r") as read_file:
+        developer = json.load(read_file)
+    res = developer["results"][int(index)]
+    return jsonify(res)
 
 if __name__ == "__main__":
     app.run(debug=True)
