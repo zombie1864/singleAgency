167811a32e56ac52a566ade8a0277a13e888c1ee
jeff <zombie1864@gmail.com>
2021-03-17 10:02:20 -0400

    tested api call with jquery and ajax

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,14 @@ from flask import Flask, json, jsonify
 
 app = Flask(__name__)
 
+@app.after_request
 def add_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] =  "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
     response.headers['Access-Control-Allow-Methods']=  "GET"
     return response
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     data = json.loads(open('fixture.json').read()) # sends json file 
     return data
