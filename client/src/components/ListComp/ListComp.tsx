a131f2ccafe42adc2a758f55f19538530028ecce
jeff <zombie1864@gmail.com>
2021-03-22 10:42:59 -0400

    added pagination - basic and bootstrap - will use later

--- client/src/components/ListComp/ListComp.tsx
+++ client/src/components/ListComp/ListComp.tsx
@@ -1,31 +1,52 @@
 import React, { Component } from 'react'
 import SearchComp from '../SearchComp'
 import appPropType from '../../types/appPropType'
+import Pagination from './Pagination'
 
-export class ListComp extends Component<appPropType> {
+interface Istate {
+    currPage: number, 
+    itemsPerPage: number 
+}
+
+export class ListComp extends Component<appPropType, Istate> {
+    constructor(props:any){
+        super(props) 
+        this.state = {
+            currPage: 1, 
+            itemsPerPage:10 
+        }
+    }
 
     public componentDidMount() {
         this.props.fetchData()
     }
+
+    private paginate = (pageNumbers:any) => this.setState({...this.state, currPage: pageNumbers})
     
     render() {
-        console.log("inside ListComp", this.props.fixture.fixture);
-
+        const indexOfLastItem = this.state.currPage * this.state.itemsPerPage
+        const indexOfFirstItem = indexOfLastItem - this.state.itemsPerPage
+        const currItems = this.props.fixture.fixture.slice(indexOfFirstItem, indexOfLastItem)
         return (
             <div>
                 <SearchComp/>
                 <h1>LIST COMP</h1>
-                <p>Data has been loaded:
-                </p>
                 <ul>
                     {
-                        this.props.fixture.fixture.map( (obj:any, idx:any) => (//[{},...,{}]
+                        currItems.map( (obj:any, idx:any) => (//[{},...,{}]
                             <li key={idx}>
                                 {obj.address}
+                                <p>bdbid: {obj.bdbid}</p>
+                                <p>building name: {obj.building_name}</p>
                             </li>
                         ))
                     }
                 </ul>
+                <Pagination 
+                    itemsPerPage={this.state.itemsPerPage}
+                    totalItems={this.props.fixture.fixture.length}
+                    paginate={this.paginate}
+                />
             </div>
         )
 
