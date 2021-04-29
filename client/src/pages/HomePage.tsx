3df66290ffa3d82c218215e62b66cc55a63ffee7
zombie1864 <zombie1864@gmail.com>
2021-04-29 14:33:45 -0400

    save currWork

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -13,7 +13,7 @@ class HomePage extends Component{
     
     return (
       <div>
-        <table>
+        <table style={{position:"absolute", top: 0, bottom: 0, left: 0, right: 0, height:"100%", width:"100%", overflow: 'hidden'}}>
           <tbody>
             <tr>
               {comps.map( (comp, idx) => {
