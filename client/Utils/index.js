df0955c04023b35e4ac260e7f28d538405336d50
zombie1864 <zombie1864@gmail.com>
2021-04-29 12:58:39 -0400

    minor changes

--- client/Utils/index.js
+++ client/Utils/index.js
@@ -11,3 +11,128 @@ export const testStore = (initialStore) => {
     const testStore = createStore(rootReducer, initialStore, applyMiddleware(...middlewares))
     return testStore
 } // createStore( reducer, initialState, func )
+
+
+export const mockData = {
+    count: 1, 
+    next: null, 
+    previous: null, 
+    results: [
+        {
+          address: "test", 
+          bdbid: 1234, 
+          building_name: "test", 
+          co2eui_breakdown: "test", 
+          energy_breakdown: "test", 
+          epapm_primary_function: "test", 
+          latitude: 0.0, 
+          longitude: 0.0, 
+          oper_agency_acronym: "test", 
+          outofservice: false, 
+          parent_record_id: "test", 
+          total_bldg_gross_sq_ft: 1900, 
+          year_built: "test" , 
+        }
+    ]
+}
+
+export const initialState = {
+    setDataReducer: {
+        obj: null, 
+        results: [
+            {
+                address: "addressTest1", 
+                bdbid: 1234, 
+                building_name: "buildingNameTest1", 
+                co2eui_breakdown: [
+                    {
+                    "bdbid": 1217,
+                    "bldg_sqft": 192000,
+                    "co2emissions_kg_sqft_site": 4.6839056,
+                    "co2emissions_kg_sqft_source": 7.383769,
+                    "fiscal_year": 2019,
+                    "id": 145000,
+                    "site_eui": 80.69169,
+                    "source_eui": 117.46657,
+                    "total_co2emissions_kg_site": 899309.8,
+                    "total_co2emissions_kg_source": 1417683.6,
+                    "total_site_energy_kbtu": 15492804,
+                    "total_source_energy_kbtu": 22553582,
+                    "updated_on": "2021-02-10T10:24:53.575000-05:00"
+                    }
+                    ], 
+                energy_breakdown: "energyBreakdownTest1", 
+                epapm_primary_function: "epapmTest1", 
+                latitude: 0.0, 
+                longitude: 0.0, 
+                oper_agency_acronym: "operTest1", 
+                outofservice: false, 
+                parent_record_id: "parentTest1", 
+                total_bldg_gross_sq_ft: 50000, 
+                year_built: "1901" , 
+            }, 
+            {
+                address: "addressTest2", 
+                bdbid: 5678, 
+                building_name: "buildingNameTest2", 
+                co2eui_breakdown: [
+                    {
+                    "bdbid": 1217,
+                    "bldg_sqft": 192000,
+                    "co2emissions_kg_sqft_site": 4.6839056,
+                    "co2emissions_kg_sqft_source": 7.383769,
+                    "fiscal_year": 2019,
+                    "id": 145000,
+                    "site_eui": 80.69169,
+                    "source_eui": 117.46657,
+                    "total_co2emissions_kg_site": 899309.8,
+                    "total_co2emissions_kg_source": 1417683.6,
+                    "total_site_energy_kbtu": 15492804,
+                    "total_source_energy_kbtu": 22553582,
+                    "updated_on": "2021-02-10T10:24:53.575000-05:00"
+                    }
+                    ], 
+                energy_breakdown: "energyBreakdownTest2", 
+                epapm_primary_function: "epapmTest2", 
+                latitude: 0.0, 
+                longitude: 0.0, 
+                oper_agency_acronym: "operTest2", 
+                outofservice: false, 
+                parent_record_id: "parentTest2", 
+                total_bldg_gross_sq_ft: 100000, 
+                year_built: "1902" , 
+            }, 
+            {
+                address: "addressTest3", 
+                bdbid: 9012, 
+                building_name: "buildingNameTest3", 
+                co2eui_breakdown: [
+                    {
+                    "bdbid": 1217,
+                    "bldg_sqft": 192000,
+                    "co2emissions_kg_sqft_site": 4.6839056,
+                    "co2emissions_kg_sqft_source": 7.383769,
+                    "fiscal_year": 2019,
+                    "id": 145000,
+                    "site_eui": 80.69169,
+                    "source_eui": 117.46657,
+                    "total_co2emissions_kg_site": 899309.8,
+                    "total_co2emissions_kg_source": 1417683.6,
+                    "total_site_energy_kbtu": 15492804,
+                    "total_source_energy_kbtu": 22553582,
+                    "updated_on": "2021-02-10T10:24:53.575000-05:00"
+                    }
+                    ], 
+                energy_breakdown: "energyBreakdownTest3", 
+                epapm_primary_function: "epapmTest3", 
+                latitude: 0.0, 
+                longitude: 0.0, 
+                oper_agency_acronym: "operTest3", 
+                outofservice: false, 
+                parent_record_id: "parentTest3", 
+                total_bldg_gross_sq_ft: 200000, 
+                year_built: "1903" , 
+            }, 
+        ]
+    }
+}
--- client/src/__test__/app.test.js
+++ client/src/__test__/app.test.js
@@ -5,39 +5,16 @@ import nock from 'nock'
 import thunk from 'redux-thunk'
 import fetch from 'isomorphic-fetch'
 import {setData} from '../actions/index'
-import Dummy from '../components/Dummy'
 import ListComp from '../components/ListComp'
 import DetailsPage from '../pages/DetailsPage'
 import App from '../app'
 import React from 'react'
-import {findByTestAttr, testStore} from '../../Utils'
+import {findByTestAttr, testStore, initialState, mockData} from '../../Utils'
 import {shallow, configure} from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 configure({ adapter: new Adapter() });
 
 //testing reducer for state change 
-const mockData = {
-    count: 1, 
-    next: null, 
-    previous: null, 
-    results: [
-        {
-          address: "test", 
-          bdbid: 1234, 
-          building_name: "test", 
-          co2eui_breakdown: "test", 
-          energy_breakdown: "test", 
-          epapm_primary_function: "test", 
-          latitude: 0.0, 
-          longitude: 0.0, 
-          oper_agency_acronym: "test", 
-          outofservice: false, 
-          parent_record_id: "test", 
-          total_bldg_gross_sq_ft: 1900, 
-          year_built: "test" , 
-        }
-    ]
-}
 
 describe('setDataReducer', () => {
     it('should return default state', () => {
@@ -120,33 +97,6 @@ describe('testing async action creator', () => {
   })
 });
 
-const setUpDemo = (props={}) => shallow(<Dummy {...props}/>)
-
-
-describe('dummy comp', () => {
-    let wrapper 
-    beforeEach(() => wrapper = setUpDemo() )
-
-    it('should render w.o err', () => {
-        const component = findByTestAttr(wrapper, 'dummyComp')
-        expect(component.length).toBe(1)
-    })
-    
-    test ('updateStateHandler method should update state', () => {
-        const classInstance = wrapper.instance()
-        classInstance.updateStateHandler()
-        const updatedState = classInstance.state.renderText
-        expect(updatedState).toBe(true)
-        
-    })
-    
-    test('returnText method should returns a str', () => {
-        const classInstance = wrapper.instance()
-        const strValue = classInstance.returnText()
-        expect(strValue).toBe('Some txt here')
-    })
-})
-
 const setUpApp = (initialState={}) => {
     const store = testStore(initialState)
     const wrapper = shallow(<App store={store}/>).childAt(0).dive()
@@ -188,107 +138,6 @@ const setUpListComp = (initialState={}) => {
     const wrapper = shallow(<ListComp store={store}/>).childAt(0).dive()
     // console.log(wrapper.debug()); // you can see the testData being rendered 
     return wrapper
-}
-
-const initialState = {
-    setDataReducer: {
-        obj: null, 
-        results: [
-            {
-                address: "addressTest1", 
-                bdbid: 1234, 
-                building_name: "buildingNameTest1", 
-                co2eui_breakdown: [
-                    {
-                    "bdbid": 1217,
-                    "bldg_sqft": 192000,
-                    "co2emissions_kg_sqft_site": 4.6839056,
-                    "co2emissions_kg_sqft_source": 7.383769,
-                    "fiscal_year": 2019,
-                    "id": 145000,
-                    "site_eui": 80.69169,
-                    "source_eui": 117.46657,
-                    "total_co2emissions_kg_site": 899309.8,
-                    "total_co2emissions_kg_source": 1417683.6,
-                    "total_site_energy_kbtu": 15492804,
-                    "total_source_energy_kbtu": 22553582,
-                    "updated_on": "2021-02-10T10:24:53.575000-05:00"
-                    }
-                    ], 
-                energy_breakdown: "energyBreakdownTest1", 
-                epapm_primary_function: "epapmTest1", 
-                latitude: 0.0, 
-                longitude: 0.0, 
-                oper_agency_acronym: "operTest1", 
-                outofservice: false, 
-                parent_record_id: "parentTest1", 
-                total_bldg_gross_sq_ft: 50000, 
-                year_built: "1901" , 
-            }, 
-            {
-                address: "addressTest2", 
-                bdbid: 5678, 
-                building_name: "buildingNameTest2", 
-                co2eui_breakdown: [
-                    {
-                    "bdbid": 1217,
-                    "bldg_sqft": 192000,
-                    "co2emissions_kg_sqft_site": 4.6839056,
-                    "co2emissions_kg_sqft_source": 7.383769,
-                    "fiscal_year": 2019,
-                    "id": 145000,
-                    "site_eui": 80.69169,
-                    "source_eui": 117.46657,
-                    "total_co2emissions_kg_site": 899309.8,
-                    "total_co2emissions_kg_source": 1417683.6,
-                    "total_site_energy_kbtu": 15492804,
-                    "total_source_energy_kbtu": 22553582,
-                    "updated_on": "2021-02-10T10:24:53.575000-05:00"
-                    }
-                    ], 
-                energy_breakdown: "energyBreakdownTest2", 
-                epapm_primary_function: "epapmTest2", 
-                latitude: 0.0, 
-                longitude: 0.0, 
-                oper_agency_acronym: "operTest2", 
-                outofservice: false, 
-                parent_record_id: "parentTest2", 
-                total_bldg_gross_sq_ft: 100000, 
-                year_built: "1902" , 
-            }, 
-            {
-                address: "addressTest3", 
-                bdbid: 9012, 
-                building_name: "buildingNameTest3", 
-                co2eui_breakdown: [
-                    {
-                    "bdbid": 1217,
-                    "bldg_sqft": 192000,
-                    "co2emissions_kg_sqft_site": 4.6839056,
-                    "co2emissions_kg_sqft_source": 7.383769,
-                    "fiscal_year": 2019,
-                    "id": 145000,
-                    "site_eui": 80.69169,
-                    "source_eui": 117.46657,
-                    "total_co2emissions_kg_site": 899309.8,
-                    "total_co2emissions_kg_source": 1417683.6,
-                    "total_site_energy_kbtu": 15492804,
-                    "total_source_energy_kbtu": 22553582,
-                    "updated_on": "2021-02-10T10:24:53.575000-05:00"
-                    }
-                    ], 
-                energy_breakdown: "energyBreakdownTest3", 
-                epapm_primary_function: "epapmTest3", 
-                latitude: 0.0, 
-                longitude: 0.0, 
-                oper_agency_acronym: "operTest3", 
-                outofservice: false, 
-                parent_record_id: "parentTest3", 
-                total_bldg_gross_sq_ft: 200000, 
-                year_built: "1903" , 
-            }, 
-        ]
-    }
 }
 
 describe("ListComp", () => {
