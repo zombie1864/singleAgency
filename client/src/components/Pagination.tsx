d7116e3adb5d15ef73cdd5c91fa9165e3ef2dba2
jeff <zombie1864@gmail.com>
2021-05-14 08:14:03 -0400

    save currWork

--- client/src/components/Pagination.tsx
+++ client/src/components/Pagination.tsx
@@ -14,7 +14,7 @@ interface Iprops {
 interface Istate {
   firstIdx: number, 
   lastIdx: number, 
-  activeKey: number, 
+  activePaginationSquare: number, 
 }
 
 class Pagination extends Component<Iprops, Istate> {
@@ -22,8 +22,8 @@ class Pagination extends Component<Iprops, Istate> {
     super(props) 
     this.state = {
       firstIdx: 1, 
-      lastIdx: 6, 
-      activeKey: 0, 
+      lastIdx: 5, 
+      activePaginationSquare: 0, 
     }
   }
 
@@ -32,25 +32,30 @@ class Pagination extends Component<Iprops, Istate> {
   }
 
   private paginationCycle = (event:any):void | null => {
-    if ( (this.state.firstIdx === 1 && event.target.name ==="prev") || (this.state.lastIdx === 11 && event.target.name === "next") ) return null // boundary conditions 
+    if ( (this.state.firstIdx === 1 && event.target.name ==="prev") || this.state.activePaginationSquare === 10 ) return null // boundary conditions 
+    if ( (this.state.lastIdx === 10 && event.target.name === "next") ) {
+      this.setState({
+        activePaginationSquare: this.state.activePaginationSquare + 1, 
+      }, () => this.props.paginate(this.state.activePaginationSquare)) 
+    }
     if (event.target.name === "prev") {
       this.props.currPage !== 1 ? this.props.paginate(this.props.currPage - 1 ) : this.props.paginate(this.state.firstIdx - 1)
       this.setState({
         firstIdx: this.state.firstIdx - 1, 
         lastIdx: this.state.lastIdx - 1, 
-        activeKey: this.state.activeKey - 1
+        activePaginationSquare: this.state.activePaginationSquare - 1
       })  
-    } else if (event.target.name === "next") {
+    } else if (event.target.name === "next" && this.state.firstIdx !== 6) {
       this.props.currPage !== 1 ? this.props.paginate(this.props.currPage + 1 ) : this.props.paginate(this.state.firstIdx + 1)
       this.setState({
         firstIdx: this.state.firstIdx + 1, 
         lastIdx: this.state.lastIdx + 1, 
-        activeKey: this.state.activeKey === 0 ? this.state.activeKey + 2 : this.state.activeKey + 1, 
+        activePaginationSquare: this.state.activePaginationSquare === 0 ? this.state.activePaginationSquare + 2 : this.state.activePaginationSquare + 1, 
       }) 
     } 
   }
 
-  private changeClassNameBasedOn = (number:number):void => this.setState({activeKey: number})
+  private changeClassNameBasedOn = (number:number):void => this.setState({activePaginationSquare: number})
   
   render() {
     const {totalItems, itemsPerPage, noResultFromSearch, searchTermPagination, searchTerm, paginate, currPage, totalSearchResultLength} = this.props
@@ -58,13 +63,15 @@ class Pagination extends Component<Iprops, Istate> {
     for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
       pageNumbers.push(i);
     }
-    console.log(this.state.firstIdx, this.state.lastIdx);
+    console.log(
+      `firstIdx: ${this.state.firstIdx}, lastIdx: ${this.state.lastIdx} \nactivePaginationSquare: ${this.state.activePaginationSquare}`
+    );
     
     let paginationRange 
     if ( searchTerm ) {
       paginationRange = noResultFromSearch === 0 ? [] : this.range(1, Math.ceil(totalSearchResultLength / itemsPerPage) )
     } else {
-      paginationRange = this.range(this.state.firstIdx, this.state.lastIdx -1)
+      paginationRange = this.range(this.state.firstIdx, this.state.lastIdx )
     }
 
     return (
@@ -73,7 +80,7 @@ class Pagination extends Component<Iprops, Istate> {
         { <button className="page-link" name="prev" onClick={this.paginationCycle}>prev</button>}
         { 
           paginationRange.map(number => { // renders pagination
-            return <li key={number} className={this.state.activeKey === number ? 'page-item active' : ''}>
+            return <li key={number} className={this.state.activePaginationSquare === number ? 'page-item active' : ''}>
               <span className='page-link paginationSpanTag' onClick={() => {
                   paginate(number) // this.props.paginate(number) => changing state to parent comp 
                   this.changeClassNameBasedOn(number)
